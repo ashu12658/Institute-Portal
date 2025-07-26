9aaff71b802adcae25320038253a8d6bff090705
ashu12658 <ashishghatol098@gmail.com>
2025-07-26 17:52:53 +0530

    Update userController.js

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -24,25 +24,6 @@ exports.registerUser = async (req, res) => {
   }
 };
 
-// Login TPO/Admin
-// exports.loginUser = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-//     const user = await User.findOne({ email });
-
-//     if (!user) return res.status(400).json({ message: 'User not found' });
-
-//     const isMatch = await bcrypt.compare(password, user.password);
-//     if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
-
-//     const token = jwt.sign({ id: user._id, role: user.role }, 'your_jwt_secret', { expiresIn: '1d' });
-
-//     res.status(200).json({ message: 'Login successful', token, user });
-//   } catch (error) {
-//     res.status(500).json({ message: 'Error logging in', error });
-//   }
-// };
-
 exports.loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
